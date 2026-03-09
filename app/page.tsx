176cdbb0b1aca6636087a17102989b060132f1ee
Celestial00 <sheerazalee223@gmail.com>
2026-03-10 00:18:21 +0500

    added components and changes per schedute

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Home() {
   return <div className="p-5">
 
-    <h1 className="text-5xl font-bold mb-2">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, perspiciatis!</h1>
+    <h1 className="text-5xl font-bold mb-1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, perspiciatis!</h1>
     <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam incidunt molestiae asperiores, ipsa minus accusamus sed, dolorem, eos nulla eum praesentium illo amet distinctio unde Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facere nesciunt alias, explicabo expedita dolore quos unde, corrupti similique ducimus asperiores voluptatem, rerum tenetur. Neque aperiam facere molestiae vero consectetur similique, accusantium reprehenderit non quaerat? Vero expedita numquam eos qui laboriosam facere! Magni architecto in veritatis totam accusantium saepe dolore dolorum!</p>
 
   </div>;
